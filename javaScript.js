aba37fc5a860cc34970198fca50ae49a0086b309
Manuelbailey101 <115514197+Manuelbailey101@users.noreply.github.com>
2022-10-29 12:55:32 -0700

    javaScript "document.write " assignment

---
+++ javaScript.js
@@ -0,0 +1,47 @@
+let age = 40;
+let country = "USA";
+let myName = "John Doe";
+let dogName ="TacoBell";
+let jobName = "Planet Fitness";
+let Email = "JohnDoe@gmail.";
+let phoneNumber = "222-444-5555";
+let Console = "Playstation";
+let favoriteFood = "Pizza";
+let Color = "red";
+let State = "california";
+let City = "San Diego";
+let fastFood = " Chick-fil-a";
+let Birthday = "December 1st";
+let favoriteState = "Hawaii";
+let favoritePlace = "My Room";
+let favoriteMovie = "HalloweenTown";
+let favoriteShow = "Lucifer";
+let secondfavoriteShow ="George Lopez";
+let favoriteCandy ="Reeces";
+
+
+document.write ("<p> " + myName + " lives in"+ country + "</p>");
+document.write (`
+<p style = "color : blue"> Name : ${myName}</p>
+<p style = "color : blue"> Age : ${age}</p>
+<p style = "color : blue"> Country : ${country}</p>
+<p style = "color : green"> TacoBell : ${dogName}</p>
+<p style = "color : green"> JobName: ${jobName}</p>
+<p style = "color : purple"> Email : ${Email}</p>
+<p style = "color : black"> Phone Number :  ${phoneNumber}</p>
+<p style = "color : red"> Console : ${Console}</p>
+<p style = "color : blue"> State : ${State}</p>
+<p style = "color : blue"> City : ${City}</p>
+<p style = "color : bright green"> FastFood : ${fastFood}</p>
+<p style = "color : brown"> Favorite candy : ${favoriteCandy}</p>
+<p style = "color : green"> birthday : ${Birthday}</p>
+<p style = "color : green"> FavoriteMovie : ${favoriteMovie}</p>
+<p style = "color : red"> favorite Color : ${Color}</p>
+<p style = "color : black"> favorite show : ${favoriteShow}</p>
+<p style = "color : green"> SecondFavoriteShow : ${secondfavoriteShow}</p>
+<p style = "color : blue"> favorite state : ${favoriteState}</p>
+<p style = "color : purple"> favorite place : ${favoritePlace}</p>
+<p style = "color : red"> favorite food : ${favoriteFood}</p>
+
+`);
+
